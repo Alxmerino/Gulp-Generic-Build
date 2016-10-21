04ebe0bc52618eee412b459bf7358284bca7d248
Rene Merino <rmerino@amayamedia.com>
2016-10-21 16:14:28 -0400

    Added SCSS build task

--- tasks/scss.js
+++ tasks/scss.js
@@ -5,11 +5,30 @@
 'use strict';
 
 let gutil = require('gulp-util');
+let sass  = require('gulp-sass');
+let autoprefixer = require('gulp-autoprefixer');
 
 function ScssTask() {
     let gulp = this;
+    let src = gulp.config.source;
+    let dest = gulp.config.dest;
 
-    return gulp;
+    return gulp.src(src.scss + '/**/*.scss')
+        .pipe(
+            sass({
+                outputStyle: 'compressed'
+            })
+            .on('error', sass.logError)
+        )
+        .pipe(
+            autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            })
+        )
+        .pipe(
+            gulp.dest(dest.css)
+        );
 }
 
 module.exports = ScssTask;
