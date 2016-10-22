e307c5e59b997da1d3d6608a600d10c6ac04e376
Rene Merino <rmerino@amayamedia.com>
2016-10-21 22:43:39 -0400

    Added empty sass doc task

---
+++ tasks/sassdoc.js
@@ -0,0 +1,15 @@
+/**
+ * Document SCSS files
+ * @author Rene Merino <rmerino@amayamedia.com>
+ */
+'use strict';
+
+let gutil = require('gulp-util');
+
+function sassDocTask() {
+    let gulp = this;
+
+    return gulp;
+}
+
+module.exports = sassDocTask;
