68024270e6861417f4f7f0aa1f90bba0b8977476
Rene Merino <rmerino@amayamedia.com>
2016-11-05 15:00:19 -0400

    Add env variables

--- tasks/scss/scss.js
+++ tasks/scss/scss.js
@@ -6,19 +6,29 @@
 'use strict';
 
 const gutil        = require('gulp-util');
+const gulpif       = require('gulp-if');
 const sass         = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps   = require('gulp-sourcemaps');
-var notify         = require("gulp-notify");
+const notify         = require("gulp-notify");
+let isProd = false;
+let isDev = true;
+
+// If "production" or "prod" is passed from the command line then update the defaults
+if(gutil.env.prod === 'prod' || gutil.env.prod === 'production') {
+    isDev = false;
+    isProd = true;
+}
 
 const sassOptions  = {
     errLogToConsole: true,
     outputStyle: 'compressed'
 };
+
 const autoprefixerOptions = {
     browsers: ['last 2 versions', '> 5%'],
     cascade: false
-}
+};
 
 function ScssTask() {
     const gulp = this;
